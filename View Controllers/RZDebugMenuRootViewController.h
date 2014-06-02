ecfd2be87d1c31af3df496bbcb0f42f2fb5c3859
Clayton R <cjrieck123@gmail.com>
2014-06-02 15:20:55 -0400

    added gesture and root view controller

---
+++ View Controllers/RZDebugMenuRootViewController.h
@@ -0,0 +1,15 @@
+//
+//  RZDebugMenuRootViewController.h
+//  RZDebugMenuDemo
+//
+//  Created by Clayton Rieck on 6/2/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RZDebugMenuRootViewController : UIViewController
+
+- (void)displayDebugMenu;
+
+@end
---
+++ View Controllers/RZDebugMenuRootViewController.m
@@ -0,0 +1,54 @@
+//
+//  RZDebugMenuRootViewController.m
+//  RZDebugMenuDemo
+//
+//  Created by Clayton Rieck on 6/2/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import "RZDebugMenuRootViewController.h"
+
+@interface RZDebugMenuRootViewController ()
+
+@end
+
+@implementation RZDebugMenuRootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    UIGestureRecognizer *doubleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(displayDebugMenu)];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)displayDebugMenu {
+    // TODO: display modal view of the menu
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
