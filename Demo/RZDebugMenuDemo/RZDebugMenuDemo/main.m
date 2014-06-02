444fbe9c26831df02fb221a1aa283f6f95119765
Clayton R <cjrieck123@gmail.com>
2014-06-02 11:28:01 -0400

    initial project structure

---
+++ Demo/RZDebugMenuDemo/RZDebugMenuDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RZDebugMenuDemo
+//
+//  Created by Clayton Rieck on 6/2/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RZAppDelegate class]));
+    }
+}
