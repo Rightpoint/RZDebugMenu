4c41e19b335380528c76a36840aa50415eef5657
Clayton Rieck <cjrieck123@gmail.com>
2014-06-03 16:29:08 -0400

    adding cell classes

---
+++ Demo/RZDebugMenuDemo/Views/RZVersionInfoTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  RZVersionInfoTableViewCell.h
+//  RZDebugMenuDemo
+//
+//  Created by Clayton Rieck on 6/3/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RZVersionInfoTableViewCell : UITableViewCell
+
+@end
