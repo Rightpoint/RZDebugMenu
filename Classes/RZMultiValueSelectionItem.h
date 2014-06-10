75663983de563dcd70945ed2915b6aba0eb1dad7
Clayton R <cjrieck123@gmail.com>
2014-06-10 15:04:27 -0400

    added properties and init method

--- Classes/RZMultiValueSelectionItem.h
+++ Classes/RZMultiValueSelectionItem.h
@@ -1,6 +1,6 @@
 //
 //  RZMultiValueSelectionItem.h
-//  RZDebugMenuDemo
+//  RZDebugMenu
 //
 //  Created by Clayton Rieck on 6/10/14.
 //  Copyright (c) 2014 Raizlabs. All rights reserved.
@@ -9,5 +9,7 @@
 #import <Foundation/Foundation.h>
 
 @interface RZMultiValueSelectionItem : NSObject
+
+- (id)initWithTitle:(NSString *)title andValue:(NSString *)value;
 
 @end
--- Classes/RZMultiValueSelectionItem.m
+++ Classes/RZMultiValueSelectionItem.m
@@ -1,6 +1,6 @@
 //
 //  RZMultiValueSelectionItem.m
-//  RZDebugMenuDemo
+//  RZDebugMenu
 //
 //  Created by Clayton Rieck on 6/10/14.
 //  Copyright (c) 2014 Raizlabs. All rights reserved.
@@ -8,6 +8,22 @@
 
 #import "RZMultiValueSelectionItem.h"
 
+@interface RZMultiValueSelectionItem ()
+
+@property(nonatomic, readonly, strong) NSString *selectionTitle;
+@property(nonatomic, readonly, strong) NSString *selectionValue;
+
+@end
+
 @implementation RZMultiValueSelectionItem
+
+- (id)initWithTitle:(NSString *)title andValue:(NSString *)value
+{
+    self = [super init];
+    if ( self ) {
+        // TODO: setup object here
+    }
+    return self;
+}
 
 @end
