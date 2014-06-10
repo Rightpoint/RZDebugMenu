69582701a98ce0222eca3f750635cc8b6e5e596e
Clayton R <cjrieck123@gmail.com>
2014-06-10 15:01:32 -0400

    added class for multi-value choice item

---
+++ Classes/RZMultiValueSelectionItem.h
@@ -0,0 +1,13 @@
+//
+//  RZMultiValueSelectionItem.h
+//  RZDebugMenuDemo
+//
+//  Created by Clayton Rieck on 6/10/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RZMultiValueSelectionItem : NSObject
+
+@end
---
+++ Classes/RZMultiValueSelectionItem.m
@@ -0,0 +1,13 @@
+//
+//  RZMultiValueSelectionItem.m
+//  RZDebugMenuDemo
+//
+//  Created by Clayton Rieck on 6/10/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import "RZMultiValueSelectionItem.h"
+
+@implementation RZMultiValueSelectionItem
+
+@end
