c248c2a9234cb3d9589942687cdd4eb453ac29b9
Clayton R <cjrieck123@gmail.com>
2014-06-11 14:25:26 -0400

    added RZDebugMenu class

---
+++ Classes/RZDebugMenu.h
@@ -0,0 +1,13 @@
+//
+//  RZDebugMenu.h
+//  RZDebugMenuDemo
+//
+//  Created by Clayton Rieck on 6/11/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RZDebugMenu : NSObject
+
+@end
---
+++ Classes/RZDebugMenu.m
@@ -0,0 +1,13 @@
+//
+//  RZDebugMenu.m
+//  RZDebugMenuDemo
+//
+//  Created by Clayton Rieck on 6/11/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import "RZDebugMenu.h"
+
+@implementation RZDebugMenu
+
+@end
