e0cbced1b78b822e582076f692d5f468e9646e24
Clayton R <cjrieck123@gmail.com>
2014-06-11 14:41:07 -0400

    create simple window that's the screen size in enable method

--- Classes/RZDebugMenu.h
+++ Classes/RZDebugMenu.h
@@ -1,6 +1,6 @@
 //
 //  RZDebugMenu.h
-//  RZDebugMenuDemo
+//  RZDebugMenu
 //
 //  Created by Clayton Rieck on 6/11/14.
 //  Copyright (c) 2014 Raizlabs. All rights reserved.
@@ -9,5 +9,7 @@
 #import <Foundation/Foundation.h>
 
 @interface RZDebugMenu : NSObject
+
++ (UIWindow *)enable;
 
 @end
--- Classes/RZDebugMenu.m
+++ Classes/RZDebugMenu.m
@@ -1,6 +1,6 @@
 //
 //  RZDebugMenu.m
-//  RZDebugMenuDemo
+//  RZDebugMenu
 //
 //  Created by Clayton Rieck on 6/11/14.
 //  Copyright (c) 2014 Raizlabs. All rights reserved.
@@ -9,5 +9,16 @@
 #import "RZDebugMenu.h"
 
 @implementation RZDebugMenu
+
++ (UIWindow *)enable
+{
+    UIScreen *mainScreen = [UIScreen mainScreen];
+    
+    CGFloat width = CGRectGetWidth(mainScreen.bounds);
+    CGFloat height = CGRectGetHeight(mainScreen.bounds);
+    
+    UIWindow *window = [[UIWindow alloc] initWithFrame:CGRectMake(0, 0, width, height)];
+    return window;
+}
 
 @end
