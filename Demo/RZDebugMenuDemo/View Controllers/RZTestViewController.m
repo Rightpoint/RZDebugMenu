2ae299f5e115a07598f28ab57934347e5a5a9852
Clayton R <cjrieck123@gmail.com>
2014-06-16 11:34:03 -0400

    cleaning up test view controller

--- Demo/RZDebugMenuDemo/View Controllers/RZTestViewController.m
+++ Demo/RZDebugMenuDemo/View Controllers/RZTestViewController.m
@@ -8,6 +8,8 @@
 
 #import "RZTestViewController.h"
 
+static NSString * const kRZTestViewControllerTitle = @"Tester";
+
 @interface RZTestViewController ()
 
 @end
@@ -18,7 +20,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        self.title = @"Tester";
+        self.title = kRZTestViewControllerTitle;
     }
     return self;
 }
@@ -27,24 +29,7 @@
 {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor redColor];
-    // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
