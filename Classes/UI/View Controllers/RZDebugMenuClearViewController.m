1eae4eab52123f86db829fb868e37f7e306dc243
Clayton R <cjrieck123@gmail.com>
2014-06-19 11:46:24 -0400

    button is draggable

--- Classes/UI/View Controllers/RZDebugMenuClearViewController.m
+++ Classes/UI/View Controllers/RZDebugMenuClearViewController.m
@@ -13,6 +13,7 @@
 @interface RZDebugMenuClearViewController ()
 
 @property (strong, nonatomic) UIButton *displayDebugMenuButton;
+@property (strong, nonatomic) UIPanGestureRecognizer *dragGesture;
 
 @end
 
@@ -22,6 +23,7 @@
 {
     self = [super init];
     if ( self ) {
+
         _delegate = delegate;
         
         _displayDebugMenuButton = [[UIButton alloc] initWithFrame:CGRectMake(50, 50, 37, 37)];
@@ -30,6 +32,8 @@
         _displayDebugMenuButton.clipsToBounds = YES;
         _displayDebugMenuButton.layer.cornerRadius = 12;
         _displayDebugMenuButton.layer.borderWidth = 1.5f;
+        
+        _dragGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(dragButton:)];
     }
     return self;
 }
@@ -40,7 +44,74 @@
     
     self.view.backgroundColor = [UIColor clearColor];
     
+    [self.displayDebugMenuButton addGestureRecognizer:self.dragGesture];
     [self.view addSubview:self.displayDebugMenuButton];
+}
+
+- (void)dragButton:(UIPanGestureRecognizer *)panGesture
+{
+    CGPoint translation = [panGesture translationInView:self.view];
+    CGPoint velocity = [panGesture velocityInView:self.view];
+    UIView *draggedButton = panGesture.view;
+    CGRect newButtonFrame = draggedButton.frame;
+    CGFloat navigationControllerHeight = self.navigationController.navigationBar.frame.size.height;
+    
+    if ( panGesture.state == UIGestureRecognizerStateChanged ) {
+        velocity = [panGesture velocityInView:self.view];
+        newButtonFrame.origin.x += translation.x;
+        newButtonFrame.origin.y += translation.y;
+        draggedButton.frame = newButtonFrame;
+        [panGesture setTranslation:CGPointZero inView:self.view];
+        
+    }
+    else if ( panGesture.state == UIGestureRecognizerStateEnded )
+    {
+        [UIView animateWithDuration:0.27
+                              delay:0.0
+                            options:UIViewAnimationOptionCurveEaseOut
+                         animations:^{
+                             CGRect newButtonFrame = draggedButton.frame;
+                             newButtonFrame.origin.x += velocity.x;
+                             newButtonFrame.origin.y += velocity.y;
+                             
+                             if ( newButtonFrame.origin.x >= CGRectGetMaxX(self.view.frame) ) {
+                                 newButtonFrame.origin.x = CGRectGetMaxX(self.view.frame) - 30;
+                             }
+                             if ( newButtonFrame.origin.y >= CGRectGetMaxY(self.view.frame) ) {
+                                 newButtonFrame.origin.y = CGRectGetMaxY(self.view.frame) - 30;
+                             }
+                             if ( newButtonFrame.origin.x <= CGRectGetMinX(self.view.frame) ) {
+                                 newButtonFrame.origin.x = CGRectGetMinX(self.view.frame) - 5;
+                             }
+                             if ( newButtonFrame.origin.y <= navigationControllerHeight) {
+                                 newButtonFrame.origin.y = navigationControllerHeight + 23;
+                             }
+                             
+                             draggedButton.frame = newButtonFrame;
+                         }
+                         completion:^(BOOL completed) {
+                             
+                             CGRect newButtonFrame = draggedButton.frame;
+                             if ( newButtonFrame.origin.x + 37 > CGRectGetMaxX(self.view.frame) ) {
+                                 newButtonFrame.origin.x = CGRectGetMaxX(self.view.frame) - 40;
+                             }
+                             
+                             if ( newButtonFrame.origin.y + 37 > CGRectGetMaxY(self.view.frame) ) {
+                                 newButtonFrame.origin.y = CGRectGetMaxY(self.view.frame) - 40;
+                             }
+                             if ( newButtonFrame.origin.x < CGRectGetMinX(self.view.frame) ) {
+                                 newButtonFrame.origin.x = CGRectGetMinX(self.view.frame) + 3;
+                             }
+                             if ( newButtonFrame.origin.y < CGRectGetMinY(self.view.frame) + self.navigationController.navigationBar.frame.size.height ) {
+                                 newButtonFrame.origin.y = CGRectGetMinY(self.view.frame) + self.navigationController.navigationBar.frame.size.height;
+                             }
+                             
+                             [UIView animateWithDuration:0.1 animations:^{
+                                 draggedButton.frame = newButtonFrame;
+                                 [panGesture setTranslation:CGPointZero inView:self.view];
+                             }];
+                         }];
+    }
 }
 
 @end
