4e786ccbc3acae1728b45905a87a6489a11913e4
Clayton R <cjrieck123@gmail.com>
2014-06-26 15:39:31 -0400

    set up init for observer

---
+++ Classes/RZDebugMenuObserver.h
@@ -0,0 +1,13 @@
+//
+//  RZDebugMenuObserver.h
+//  RZDebugMenuDemo
+//
+//  Created by Clayton Rieck on 6/26/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RZDebugMenuObserver : NSObject
+
+@end
---
+++ Classes/RZDebugMenuObserver.m
@@ -0,0 +1,22 @@
+//
+//  RZDebugMenuObserver.m
+//  RZDebugMenuDemo
+//
+//  Created by Clayton Rieck on 6/26/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import "RZDebugMenuObserver.h"
+
+@implementation RZDebugMenuObserver
+
+- (id)init
+{
+    self = [super init];
+    if ( self ) {
+        
+    }
+    return self;
+}
+
+@end
