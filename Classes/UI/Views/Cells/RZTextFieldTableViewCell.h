094781fa861da5df2753a56e3c949e45881db21c
Clayton R <cjrieck123@gmail.com>
2014-06-30 09:10:59 -0400

    added cell for text field

---
+++ Classes/UI/Views/Cells/RZTextFieldTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  RZTextFieldTableViewCell.h
+//  RZDebugMenuDemo
+//
+//  Created by Clayton Rieck on 6/30/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RZTextFieldTableViewCell : UITableViewCell
+
+@property (strong, nonatomic) UILabel *stringLabel;
+
+@end
---
+++ Classes/UI/Views/Cells/RZTextFieldTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  RZTextFieldTableViewCell.m
+//  RZDebugMenuDemo
+//
+//  Created by Clayton Rieck on 6/30/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import "RZTextFieldTableViewCell.h"
+
+@implementation RZTextFieldTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:reuseIdentifier];
+    if ( self ) {
+        _stringLabel = [[UILabel alloc] init];
+        self.accessoryView = [[UIView alloc] initWithFrame:_stringLabel.frame];
+    }
+    
+    return self;
+}
+
+@end
