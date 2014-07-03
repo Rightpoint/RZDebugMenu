c3998f03bd3db61061fa70609b2191402158523e
Clayton R <cjrieck123@gmail.com>
2014-07-03 13:36:17 -0400

    add error log

--- Demo/RZLoggingMacros.h
+++ Demo/RZLoggingMacros.h
@@ -9,6 +9,10 @@
 #ifndef RZDebugMenuDemo_RZLoggingMacros_h
 #define RZDebugMenuDemo_RZLoggingMacros_h
 
+#if DEBUG
 #define RZDebugMenuLogDebug_log( s, ... ) NSLog((@"[RZDebugMenu]: DEBUG --  (s)"), ##__VA_ARGS__)
+#endif
+
+#define RZDebugMenuErrorLog( s, ... ) NSLog((@"[RZDebugMenu]: ERROR -- (s)"). ##__VA_ARGS__)
 
 #endif
