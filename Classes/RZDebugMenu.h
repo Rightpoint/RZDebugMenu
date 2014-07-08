8b8bc763955fb6ce6fb70f4c0aaf9fa33596c2f6
Clayton R <cjrieck123@gmail.com>
2014-07-08 11:54:35 -0400

    added appledocs for RZDebugMenu.h

--- Classes/RZDebugMenu.h
+++ Classes/RZDebugMenu.h
@@ -17,9 +17,32 @@ OBJC_EXTERN NSString* const kRZDebugMenuSettingChangedNotification;
 
 @interface RZDebugMenu : NSObject
 
+/**
+ Enables the debug menu with a specified settings plist
+ @param fileName Name of the plist which have the settings definitions as NSString
+ */
 + (void)enableWithSettingsPlist:(NSString *)fileName;
+
+/**
+ Returns the setting value for a specified setting key defined in the plist
+ @param key Key as defined in the plist under the 'Key' field
+ @return An object that is the value of the indexed setting
+ */
 + (id)debugSettingForKey:(NSString *)key;
+
+/**
+ Sets an object to observe changes for a particular setting
+ @param observer Object to observe changes
+ @param aSelector A selector to perform when a setting that an object is observing changes
+ @param key The key whose value a change will be observed for
+ */
 + (void)addObserver:(id)observer selector:(SEL)aSelector forKey:(NSString *)key;
+
+/**
+ Removes an object currently observing for a settings change
+ @param observer Object you wish to remove from the observer pool
+ @param key The key the observer was observing a change on
+ */
 + (void)removeObserver:(id)observer forKey:(NSString *)key;
 
 @end
