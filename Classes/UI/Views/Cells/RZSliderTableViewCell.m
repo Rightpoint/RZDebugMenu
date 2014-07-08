34fa3cdbd2440375e675b8a61d9d06ad694359dd
Clayton R <cjrieck123@gmail.com>
2014-07-08 08:51:45 -0400

    center slider with margin on each side

--- Classes/UI/Views/Cells/RZSliderTableViewCell.m
+++ Classes/UI/Views/Cells/RZSliderTableViewCell.m
@@ -16,7 +16,9 @@
     if (self) {
         CGFloat width = CGRectGetWidth(self.bounds);
         CGFloat height = CGRectGetHeight(self.bounds);
-        _cellSlider = [[UISlider alloc] initWithFrame:CGRectMake(0, 0, width, height)];
+        CGFloat const sliderMargin = 25.0;
+        _cellSlider = [[UISlider alloc] initWithFrame:CGRectMake(0, 0, width-sliderMargin, height)];
+        _cellSlider.center = CGPointMake(CGRectGetMidX(self.contentView.bounds), CGRectGetMidY(self.contentView.bounds));
         _cellSlider.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
         [_cellSlider addTarget:self action:@selector(changedSlider) forControlEvents:UIControlEventValueChanged];
         [self addSubview:_cellSlider];
