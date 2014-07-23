b97ca786253ca3b23f2b7d70f34ee52f8af87246
Clayton R <cjrieck123@gmail.com>
2014-07-23 14:11:32 -0400

    add test for debug menu

---
+++ Demo/RZDebugMenuDemoTests/RZDebugMenuTests.m
@@ -0,0 +1,35 @@
+//
+//  RZDebugMenuTests.m
+//  RZDebugMenuDemo
+//
+//  Created by Clayton Rieck on 7/23/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "RZDebugMenu.h"
+
+@interface RZDebugMenuTests : XCTestCase
+
+@end
+
+@implementation RZDebugMenuTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testRZDebugMenuInstance
+{
+    
+}
+
+@end
